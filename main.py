ac42cd4e3eeea098af912552c1a471d0d93818ce
Hallel <halleli100@gmail.com>
2023-01-24 22:50:36 +0200

    add classification_report

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score, recall_score
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+
+def main():
+    mnist = load_digits()
+    target = mnist["target"]
+    images = mnist["images"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        flatten_img(images), target, test_size=0.3, shuffle=True
+    )
+
+    y_train_5 = (y_train == 5)
+    y_test_5 = (y_test == 5)
+    predicted = binary_class(X_train, X_test, y_train_5)
+    meause(y_test_5, predicted)
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        flatten_img(images), target, test_size=0.3, shuffle=True
+    )
+
+    multi_class_sgd(X_train, X_test, y_train)  # todo: why the results are so bad??
+    meause(y_test, predicted)
+
+    # todo: why the result the same of both the training without the split here?
+    X_train, X_test, y_train, y_test = train_test_split(
+        flatten_img(images), target, test_size=0.3, shuffle=True
+    )
+    multi_class_clf(X_train, X_test, y_train)  # todo: why the results are so bad??
+    meause(y_test, predicted)
 
 
 def multi_class_clf(X_train, X_test, y_train):
@@ -30,36 +60,23 @@ def binary_class(X_train, X_test, y_train):
     return sgd_clf.predict(X_test)
 
 
-def meause(y_test, predicted):
-    print("confusion_matrix: ", confusion_matrix(y_test, predicted))
-    print("Precision Score : ", precision_score(y_test, predicted, average='micro'))
-    print("Recall Score : ", recall_score(y_test, predicted, average='micro'))
+def meause(y_real, predicted):
+    # todo: why prec anf recall are the same always?
+    print("confusion_matrix: ", confusion_matrix(y_real, predicted))
+    print("Precision Score : ", precision_score(y_real, predicted, average='micro'))
+    print("Recall Score : ", recall_score(y_real, predicted, average='micro'))
+    no_pred = set(y_real) - set(predicted)
+    print(f"no predicts for labels: {no_pred}")
+    print(
+        f"Classification report for classifier :\n"
+        f"{classification_report(y_real, predicted,labels= list(set(predicted)))}\n"
+    )
+    print("\n\n")
 
 
 def flatten_img(images):
     n_samples = len(images)
     return images.reshape((n_samples, -1))
-
-
-def main():
-    mnist = load_digits()
-    target = mnist["target"]
-    images = mnist["images"]
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        flatten_img(images), target, test_size=0.3, shuffle=True
-    )
-
-    y_train_5 = (y_train == 5)
-    y_test_5 = (y_test == 5)
-    predicted = binary_class(X_train, X_test, y_train_5)
-    meause(y_test_5, predicted)
-
-    multi_class_sgd(X_train, X_test, y_train)
-    meause(y_test, predicted)
-
-    binary_class(X_train, X_test, y_train)
-    meause(y_test, predicted)
 
 
 if __name__ == "__main__":
